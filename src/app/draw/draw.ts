4f035716775755e074468ded0c140a828df57fc7
RSamaium <sam@newbot.io>
2025-11-24 14:13:08 +0100

    draw

--- src/app/draw/draw.ts
+++ src/app/draw/draw.ts
@@ -1,27 +1,42 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, ElementRef, inject, OnInit, PLATFORM_ID, viewChild } from '@angular/core';
+import { afterEveryRender, afterNextRender, Component, effect, ElementRef, inject, OnInit, PLATFORM_ID, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-draw',
   imports: [],
   template: ` <canvas #canvasRef></canvas> `,
 })
-export class Draw implements OnInit {
+export class Draw {
   private platformId = inject(PLATFORM_ID);
 
   canvasEl = viewChild<ElementRef<HTMLCanvasElement>>('canvasRef');
 
-  ngOnInit(): void {
-    if (!isPlatformBrowser(this.platformId)) {
-      return;
-    }
-    const el = this.canvasEl()?.nativeElement;
-    if (el) {
-      const context = el.getContext('2d');
-      if (context) {
-        context.fillStyle = 'blue';
-        context.fillRect(0, 0, 100, 100);
+  constructor() {
+    // afterNextRender: ne s'exécute jamais côté serveur, s'execute qu'une fois
+    // afterEveryRender: ne s'exécute jamais côté serveur, s'execute plusieurs fois (ancien nom: afterRender)
+    afterEveryRender(() => {
+      const el = this.canvasEl()?.nativeElement;
+      if (el) {
+        const context = el.getContext('2d');
+        if (context) {
+          context.fillStyle = 'blue';
+          context.fillRect(0, 0, 100, 100);
+        }
       }
-    }
+    })
   }
+
+  // ngOnInit(): void {
+  //   if (!isPlatformBrowser(this.platformId)) {
+  //     return;
+  //   }
+  //   const el = this.canvasEl()?.nativeElement;
+  //   if (el) {
+  //     const context = el.getContext('2d');
+  //     if (context) {
+  //       context.fillStyle = 'blue';
+  //       context.fillRect(0, 0, 100, 100);
+  //     }
+  //   }
+  // }
 }
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 const age = signal(18)
+
+
+effect(() => {
+    console.log(age()) // 18, 20
+})
+
 age.set(20)
-age()
