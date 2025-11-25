23aac3b83ec28baf0d588768a387259d7ef82249
RSamaium <sam@newbot.io>
2025-11-25 15:15:25 +0100

    server ts

--- src/server.ts
+++ src/server.ts
@@ -1,68 +1,33 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
 import { join } from 'node:path';
+import express from 'express'
+import { AngularNodeAppEngine, createNodeRequestHandler, writeResponseToNodeResponse } from '@angular/ssr/node';
+ 
+const app = express()
+const angularApp = new AngularNodeAppEngine()
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+const browserDistFolder = join(import.meta.dirname, '/../browser')
 
-const app = express();
-const angularApp = new AngularNodeAppEngine();
-
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
-
-/**
- * Serve static files from /browser
- */
 app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
+  express.static(browserDistFolder)
+)
 
-/**
- * Handle all other requests by rendering the Angular application.
- */
-app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
-
-/**
- * Start the server if this module is the main entry point, or it is ran via PM2.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
+app.use(async (req, res, next) => {
+  try {
+    const angularRes = await angularApp.handle(req)
+    if (angularRes) {
+      await writeResponseToNodeResponse(angularRes, res)
     }
+    else {
+      next()
+    }
+  }
+  catch (err) {
+    next(err)
+  }
+})
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+// app.listen(process.env['PORT'] ?? 3000, () => {
+//   console.log('serveur démarré')
+// })
 
-/**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
- */
-export const reqHandler = createNodeRequestHandler(app);
+export const reqHandler = createNodeRequestHandler(app)
