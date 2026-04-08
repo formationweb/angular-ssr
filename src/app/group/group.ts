12e5ae3517896945ae760ee0c93559e4c1c802d9
RSamaium <sam@newbot.io>
2026-04-08 16:51:17 +0200

    group

--- src/app/group/group.ts
+++ src/app/group/group.ts
@@ -1,14 +1,36 @@
-import { Component, inject, REQUEST_CONTEXT, signal } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, inject, makeStateKey, PLATFORM_ID, REQUEST_CONTEXT, signal, TransferState } from '@angular/core';
+
+const GROUP_TOKEN = makeStateKey<'a' | 'b' | null>('group')
 
 @Component({
   selector: 'app-group',
   imports: [],
-  templateUrl: './group.html',
-  styleUrl: './group.css',
+  template: `
+    @if (group() == 'a') {
+      <div>A</div>
+    }
+    @else  {
+      <div>B</div>
+    }
+  `,
 })
 export class Group {
   private requestContext = inject<{
     group: string
   } | null>(REQUEST_CONTEXT)
+  private platformId = inject(PLATFORM_ID)
+  private transferState = inject(TransferState)
+  private isServer = isPlatformServer(this.platformId)
   group = signal(this.requestContext?.group)
+
+  constructor() {
+    const groupCached = this.transferState.get(GROUP_TOKEN, null)
+    if (this.isServer) {
+      this.transferState.set(GROUP_TOKEN, this.group())
+    }
+    if (groupCached) {
+      this.group.set(groupCached)
+    }
+  }
 }
